23a7bc52de3abe761c471e19b4f17c46fc11375a
Anup Kumar Panwar <mylnext@gmail.com>
2016-08-24 14:35:25 +0530

    Android app

---
+++ ChatBurn/app/src/main/java/com/anupkumarpanwar/chatburn/MainActivity.java
@@ -0,0 +1,37 @@
+package com.anupkumarpanwar.chatburn;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private WebView mWebview ;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        mWebview  = new WebView(this);
+
+        mWebview.getSettings().setJavaScriptEnabled(true); // enable javascript
+
+        final Activity activity = this;
+
+        mWebview.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+                mWebview.loadUrl("file:///android_asset/error.html");
+//                setContentView(mWebview);
+            }
+        });
+
+        mWebview .loadUrl("http://www.chatburn.16mb.com");
+        setContentView(mWebview );
+
+    }
+
+}
